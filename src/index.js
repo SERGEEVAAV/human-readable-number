43c734e57c97f57ca66b285f3bebef29d31b7221
unknown <Anastasiapv@bk.ru>
2022-08-12 16:31:10 +0300

    feat: Added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function toReadable (number) {
-  
+ 
+  let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  let teens = ['ten', 'eleven', 'twelve', 'thirhteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eigthy', 'ninety'];
+
+  let numberString = number.toString();
+
+  if (number === 0){
+    return 'zero';
+  }
+  if(number < 10) {
+    return ones[number];
+  }
+  if(number >= 10 && number <= 19) {
+    return teens[numberString[1]];
+  }
+  if(number.lenght == 2 && numberString[0] >= 2) {
+    return tens[numberString[0]] + ' ' + ones[numberString[1]];
+  }
 }
