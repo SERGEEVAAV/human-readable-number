8e007789e08855c57cd397119244b8fb26c863d8
unknown <Anastasiapv@bk.ru>
2022-08-12 20:28:42 +0300

    fix: latest calculations

--- src/index.js
+++ src/index.js
@@ -13,19 +13,19 @@ module.exports = function toReadable (number) {
     return ones[number];
   }
   if(number >= 10 && number <= 19) {
-    return teens[numberString[1]];
+    return (teens[numberString[1]]).trim();
   }
 
   if(numberString.length === 2 && numberString[0] >= 2){
-    return tens[numberString[0]]  + ' ' + ones[numberString[1]];
+    return (tens[numberString[0]]  + ' ' + ones[numberString[1]]).trim();
   }
   if(numberString.length === 3) {
     if(numberString[1] == 0 && numberString[2] == 0) {
-      return ones[numberString[0]] + ' hundred ';
+      return (ones[numberString[0]] + ' hundred ').trim();
     } else if(numberString[1] == 0) {
-      return ones[numberString[0]] + ' hundred ' + ones[numberString[2]];
+      return (ones[numberString[0]] + ' hundred ' + ones[numberString[2]]).trim();
     }else if (numberString[1] == 1) {
-      return ones[numberString[0]] + ' hundred ' + teens[numberString[2]];
+      return (ones[numberString[0]] + ' hundred ' + teens[numberString[2]]).trim();
     } else {
       return (ones[numberString[0]] + ' hundred ' + tens[numberString[1]] + ' ' + ones[numberString[2]]).trim();
     }
